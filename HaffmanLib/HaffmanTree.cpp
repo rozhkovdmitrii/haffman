ec2e4f8b01013b28e21a32ffd6a39d2eb9443080
droz <rozhkov@iskrauraltel.ru>
2019-01-18 18:52:06 +0500

    Add delimiters

--- HaffmanLib/HaffmanTree.cpp
+++ HaffmanLib/HaffmanTree.cpp
@@ -1,37 +1,37 @@
+//----------------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <queue>
 #include <algorithm>
-
+//----------------------------------------------------------------------------------------------------------------------
 #include "Log.h"
 #include "HaffmanTree.h"
-
+//----------------------------------------------------------------------------------------------------------------------
 namespace HaffmanImpl
 {
-
+//----------------------------------------------------------------------------------------------------------------------
 JoinNode::JoinNode(TreeNode * left, TreeNode * right) :
   TreeNode(Type::Join),
   _left(left), _right(right)
 {
   calcFrequency();
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 JoinNode::JoinNode() : JoinNode(nullptr, nullptr) {
 }
-
-
+//----------------------------------------------------------------------------------------------------------------------
 JoinNode::~JoinNode()
 {
   delete _left;
   delete _right;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void JoinNode::calcFrequency()
 {
   _freq = (_left ? _left->getFreq() : 0) + (_right ? _right->getFreq() : 0);
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 bool JoinNode::setLeft(TreeNode * node)
 {
   if (_left != nullptr)
@@ -40,7 +40,7 @@ bool JoinNode::setLeft(TreeNode * node)
   calcFrequency();
   return true;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 bool JoinNode::setRight(TreeNode * node)
 {
   if (_right != nullptr)
@@ -49,7 +49,7 @@ bool JoinNode::setRight(TreeNode * node)
   calcFrequency();
   return true;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void JoinNode::setCode(const TreeCode & code)
 {
   TreeNode::setCode(code);
@@ -58,17 +58,17 @@ void JoinNode::setCode(const TreeCode & code)
   if (_right)
     _right->setCode(_code.getCodeToTheRight());
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 TreeNode * JoinNode::getLeft() const
 {
   return _left;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 TreeNode * JoinNode::getRight() const
 {
   return _right;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 std::string JoinNode::toString() const
 {
   std::ostringstream ostream;
@@ -76,7 +76,7 @@ std::string JoinNode::toString() const
           << _freq << ")";
   return ostream.str();
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 bool JoinNode::put(TreeNode * node) {
   if (node == nullptr)
     return LOG(DBGERR) << "cannot put nullptr node into HaffmanImpl Tree";
@@ -84,30 +84,29 @@ bool JoinNode::put(TreeNode * node) {
     return true;
   return LOG(DBGERR) << "cannot put node into full HaffmanImpl Tree";
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 bool JoinNode::isFull() const {
   return _left != nullptr && _right != nullptr;
 }
-
-
+//----------------------------------------------------------------------------------------------------------------------
 TreeNode::Type TreeNode::getType() const
 {
   return _type;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 uint TreeNode::getFreq() const
 {
   return _freq;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void TreeNode::setFreq(uint freq)
 {
   _freq = freq;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 TreeCode::TreeCode(byte base, int size) : _base(base), _size(size) {}
 TreeCode::TreeCode(const TreeCode & code) : TreeCode(code._base, code._size) {}
-
+//----------------------------------------------------------------------------------------------------------------------
 TreeCode TreeCode::getCodeToTheRight() const {
   TreeCode codeToTheRight(*this);
   codeToTheRight._base <<= 1;
@@ -115,51 +114,51 @@ TreeCode TreeCode::getCodeToTheRight() const {
   ++codeToTheRight._size;
   return codeToTheRight;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 TreeCode TreeCode::getCodeToTheLeft() const {
   TreeCode codeToTheLeft(*this);
   codeToTheLeft._base <<= 1;
   ++codeToTheLeft._size;
   return codeToTheLeft;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 bool TreeCode::operator==(const TreeCode & right) const {
   return _size == right._size && _base == right._base;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void TreeNode::setCode(const TreeCode & code) {
   _code = code;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 std::string LeafNode::toString() const {
   std::ostringstream ostream;
   ostream << "(" << _sym << _freq << ")";
   return ostream.str();
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 const TreeCode & LeafNode::getCode() const {
   return _code;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 const byte LeafNode::getSym() const {
   return _sym;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void LeafNode::setSym(byte sym) {
   _sym = sym;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 HaffmanTree::HaffmanTree(const VecFreqItem & vecFreqItem) {
 
   buildFrom(vecFreqItem);
   updateCachedCodes(_top);
   indexTree();
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 HaffmanTree::~HaffmanTree() {
   delete _top;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void HaffmanTree::buildFrom(const VecFreqItem & vecFreqItem) {
   reset();
 
@@ -188,7 +187,7 @@ void HaffmanTree::buildFrom(const VecFreqItem & vecFreqItem) {
   }
   _top = join;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void HaffmanTree::updateCachedCodes(TreeNode * treeNode) {
   if (treeNode == nullptr)
     return;
@@ -203,7 +202,7 @@ void HaffmanTree::updateCachedCodes(TreeNode * treeNode) {
   if (leaf != nullptr)
     _rawLeafNodes[leaf->getSym()] = leaf;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void HaffmanTree::indexTree() {
   if (!_top)
   {
@@ -212,7 +211,7 @@ void HaffmanTree::indexTree() {
   }
   _top->setCode(TreeCode());
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 void HaffmanTree::reset() {
   std::fill(_rawLeafNodes.begin(), _rawLeafNodes.end(), nullptr);
   if (_top == nullptr)
@@ -220,23 +219,22 @@ void HaffmanTree::reset() {
   delete _top;
   _top = nullptr;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 JoinNode * HaffmanTree::getTop() const {
   return _top;
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 std::string HaffmanTree::toString() const {
   return (_top == nullptr) ? "()" : _top->toString();
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 const TreeCode & HaffmanTree::getCode(byte sym) const {
   const static TreeCode empty;
   return _rawLeafNodes[sym] == nullptr ? empty : _rawLeafNodes[sym]->getCode();
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 HaffmanTree::HaffmanTree() : _top(nullptr) {}
-
-#include <algorithm>
+//----------------------------------------------------------------------------------------------------------------------
 HaffmanTree::HaffmanTree(HaffmanTree && haffmanTree) noexcept {
   reset();
   _top = haffmanTree._top;
@@ -244,7 +242,7 @@ HaffmanTree::HaffmanTree(HaffmanTree && haffmanTree) noexcept {
   haffmanTree._top = nullptr;
   std::copy(haffmanTree._rawLeafNodes.begin(), haffmanTree._rawLeafNodes.end(), _rawLeafNodes.begin());
 }
-
+//----------------------------------------------------------------------------------------------------------------------
 HaffmanTree & HaffmanTree::operator=(HaffmanTree && haffmanTree) noexcept {
   reset();
   _top = haffmanTree._top;
@@ -252,8 +250,7 @@ HaffmanTree & HaffmanTree::operator=(HaffmanTree && haffmanTree) noexcept {
   haffmanTree._top = nullptr;
   return *this;
 }
-
-
+//----------------------------------------------------------------------------------------------------------------------
 std::ostream & operator<<(std::ostream & os, const TreeCode & treeCode) {
   int size = treeCode._size;
   while (size)
@@ -263,4 +260,6 @@ std::ostream & operator<<(std::ostream & os, const TreeCode & treeCode) {
   }
   return os;
 }
+//----------------------------------------------------------------------------------------------------------------------
 }
+//----------------------------------------------------------------------------------------------------------------------
